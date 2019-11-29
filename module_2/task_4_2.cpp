1bc3e2dca4b36fa8b738f4ca277eb605f990ad8a
k-t-l-h <45423059+k-t-l-h@users.noreply.github.com>
2019-11-29 18:54:39 +0300

    RunID 26763906

--- module_2/task_4_2.cpp
+++ module_2/task_4_2.cpp
@@ -1,223 +1,223 @@
-/*
-Порядковые статистики. Дано число N и N строк. Каждая строка содержит
-команду добавления или удаления натуральных чисел, а также запрос на
-получение k-ой порядковой статистики. Команда добавления числа A
-задается положительным числом A, команда удаления числа A задается
-отрицательным числом “-A”. Запрос на получение k-ой порядковой
-статистики задается числом k.
-*/
-
-
 #include <iostream>
 
+template<class Value>
 class AVL
 {
+	class Node {
 	public:
+		Node(Value data) : data(data), left(0), right(0), height(1), number(1) {}
+		Value data;
+		Node *left, *right;
+		int height;
+		int number;
+	};
+public:
 
-	AVL(): data(0), height(1), number(1), left(nullptr), right(nullptr) {};
-	AVL(int data): data(data), height(1), number(1), left(nullptr), right(nullptr) {};
+	AVL() {
+		root = NULL;
+	};
 
-	int data;
-	int height;
-	int number;
-
-	AVL* left;
-	AVL* right;
-
-};
-
-int get_height(AVL* tree)
-{
-	return tree? tree->height : 0;
-};
-
-
-int get_balance_factor(AVL* tree)
-{
-	return get_height(tree->right) - get_height(tree->left);
-};
-
-void renum(AVL* tree)
-{
-	int h_left = get_height(tree->left);
-	int h_right = get_height(tree->right);
-
-	h_left > h_right ? tree->height = h_left + 1 : tree->height = h_right + 1;
-
-	int n_left = tree->left ? tree->left->number: 0;
-	int n_right = tree->right ? tree->right->number: 0;
-	tree->number = n_left + n_right + 1;
-
-};
-
-AVL* small_right(AVL* tree)
-{
-	AVL* tmp = tree->left;
-	tree->left = tmp->right;
-	tmp->right = tree;
-
-	renum(tree);
-	renum(tmp);
-
-	return tmp;
-};
-
-AVL* small_left(AVL* tree)
-{
-	AVL* tmp = tree->right;
-	tree->right = tmp->left;
-	tmp->left = tree;
-
-	renum(tree);
-	renum(tmp);
-
-	return tmp;
-};
-
-
-AVL* check_balance(AVL* tree)
-{
-	renum(tree);
-
-	int factor = get_balance_factor(tree);
-
-	if (factor == 2)
+	int kstat(int k)
 	{
-		if(get_balance_factor(tree->right) < 0)
+		Node* current = root;
+		int t = 0;
+
+		while (current)
 		{
-			tree->right = small_right(tree->right);
+			t = current->left ? current->left->number : 0;
+
+			if (t == k)
+			{
+				return current->data;
+			}
+
+			if (t > k)
+			{
+				current = current->left;
+			}
+			else
+			{
+				current = current->right;
+				k = k - 1 - t;
+			}
 		}
-		return small_left(tree);
+		return -1;
+
+	};
+
+	void insertAVL(int data) {
+		root = insert(root, data);
 	}
 
-	if (factor == -2)
+	void removeAVL(int data) {
+		root = remove(root, data);
+	}
+
+private:
+	int get_height(Node* node)
 	{
-		if(get_balance_factor(tree->left) > 0)
+		return node ? node->height : 0;
+	};
+
+	int get_balance_factor(Node* node)
+	{
+		return get_height(node->right) - get_height(node->left);
+	};
+
+	void renum(Node* node)
+	{
+		int h_left = get_height(node->left);
+		int h_right = get_height(node->right);
+
+		h_left > h_right ? node->height = h_left + 1 : node->height = h_right + 1;
+
+		int n_left = node->left ? node->left->number : 0;
+		int n_right = node->right ? node->right->number : 0;
+		node->number = n_left + n_right + 1;
+
+	};
+
+	Node* small_right(Node* node)
+	{
+		Node* tmp = node->left;
+		node->left = tmp->right;
+		tmp->right = node;
+
+		renum(node);
+		renum(tmp);
+
+		return tmp;
+	};
+
+	Node* small_left(Node* node)
+	{
+		Node* tmp = node->right;
+		node->right = tmp->left;
+		tmp->left = node;
+
+		renum(node);
+		renum(tmp);
+
+		return tmp;
+	};
+
+
+	Node* check_balance(Node* node)
+	{
+		renum(node);
+
+		int factor = get_balance_factor(node);
+
+		if (factor == 2)
 		{
-			tree->left = small_left(tree->left);
-		}
-		return small_right(tree);
-	}
-	return tree;
-};
-
-AVL* insert(AVL* tree, int data)
-{
-	if(!tree)
-		return new AVL(data);
-
-	if( data < tree->data)
-		tree->left = insert(tree->left, data);
-	else
-		tree->right = insert(tree->right, data);
-
-    tree = check_balance(tree);
-
-	return tree;
-};
-
-AVL* find_min(AVL* tree)
-{
-	return tree->right ? find_min(tree->right) : tree;
-};
-
-AVL* remove_min(AVL* tree)
-{
-	if(!tree->right)
-		return tree->left;
-
-	tree->right = remove_min(tree->right);
-
-	return check_balance(tree);
-};
-
-AVL* remove(AVL* tree, int data)
-{
-	if (!tree)
-		return tree;
-
-	if( tree->data > data)
-	{
-		tree->left = remove(tree->left, data);
-	}
-
-	if( tree->data < data)
-	{
-		tree->right = remove(tree->right, data);
-	}
-
-	if(tree->data == data)
-	{
-		AVL* l = tree->left;
-		AVL* r = tree->right;
-		delete tree;
-
-		if (!l)
-			return r;
-
-		AVL* tmp = find_min(l);
-		tmp->left = remove_min(l);
-		tmp->right = r;
-
-		return check_balance(tmp);
-	}
-
-	return check_balance(tree);
-};
-
-
-int kstat(AVL* tree, int k)
-{
-	AVL* current = tree;
-	int t = 0;
-
-	while(current)
-	{
-		t = current->left ? current->left->number : 0;
-
-		if (t == k)
-		{
-			return current->data;
+			if (get_balance_factor(node->right) < 0)
+			{
+				node->right = small_right(node->right);
+			}
+			return small_left(node);
 		}
 
-		if( t > k )
+		if (factor == -2)
 		{
-			current = current->left;
+			if (get_balance_factor(node->left) > 0)
+			{
+				node->left = small_left(node->left);
+			}
+			return small_right(node);
 		}
+		return node;
+	};
+
+	Node* insert(Node* node, int data)
+	{
+		if (!node)
+			return new Node(data);
+
+		if (data < node->data)
+			node->left = insert(node->left, data);
 		else
+			node->right = insert(node->right, data);
+
+		node = check_balance(node);
+
+		return node;
+	};
+
+	Node* find_min(Node* node)
+	{
+		return node->right ? find_min(node->right) : node;
+	};
+
+	Node* remove_min(Node* node)
+	{
+		if (!node->right)
+			return node->left;
+
+		node->right = remove_min(node->right);
+
+		return check_balance(node);
+	};
+
+	Node* remove(Node* node, int data)
+	{
+		if (!node)
+			return node;
+
+		if (node->data > data)
 		{
-			current = current->right;
-			k = k - 1 - t;
+			node->left = remove(node->left, data);
 		}
-	}
-	return - 1;
 
+		if (node->data < data)
+		{
+			node->right = remove(node->right, data);
+		}
+
+		if (node->data == data)
+		{
+			Node* l = node->left;
+			Node* r = node->right;
+			delete node;
+
+			if (!l)
+				return r;
+
+			Node* tmp = find_min(l);
+			tmp->left = remove_min(l);
+			tmp->right = r;
+
+			return check_balance(tmp);
+		}
+
+		return check_balance(node);
+	};
+
+	Node* root;
 };
-
 
 int main() {
 
-    AVL *tree = nullptr;
+	AVL<int> tree;
 
-    int N;
-    std::cin >> N;
+	int N;
+	std::cin >> N;
 
 	int value = 0;
 	int k = 0;
 
-    for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		std::cin >> value >> k;
 		if (value < 0)
 		{
-			tree = remove(tree, std::abs(value));
+			tree.removeAVL(std::abs(value));
 		}
 		else
 		{
-			tree = insert(tree, value);
+			tree.insertAVL(value);
 		}
-		std::cout << kstat(tree, k) << " ";
-    }
+		std::cout << tree.kstat(k) << " ";
+	}
 
-    return 0;
+	return 0;
 }
